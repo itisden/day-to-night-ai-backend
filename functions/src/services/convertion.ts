ff5ac1e695c2119c836b982fc1ffdf1e84651355
itisden <denispulyaev@gmail.com>
2023-09-22 13:37:11 +0300

    remove replicate token from source code

--- functions/src/services/convertion.ts
+++ functions/src/services/convertion.ts
@@ -1,22 +1,21 @@
 import Replicate from "replicate";
 
 const replicate = new Replicate({
-  auth: "r8_QyCPOMeRzNrOkYfJa5trXp97zTSea1p3VwVTa",
+  auth: process.env.REPLICATE_API_TOKEN || "",
 });
 
 export const convertImage = async (image: string) => {
   // eslint-disable-next-line max-len
-  const model = "lucataco/sdxl-controlnet:db2ffdbdc7f6cb4d6dab512434679ee3366ae7ab84f89750f8947d5594b79a47";
+  const model =
+    // eslint-disable-next-line max-len
+    "lucataco/sdxl-controlnet:db2ffdbdc7f6cb4d6dab512434679ee3366ae7ab84f89750f8947d5594b79a47";
 
-  const prediction = await replicate.run(
-    model,
-    {
-      input: {
-        image,
-        prompt: "a picture of a house at night",
-      },
-    }
-  ) as Promise<string>;
+  const prediction = (await replicate.run(model, {
+    input: {
+      image,
+      prompt: "a picture of a house at night",
+    },
+  })) as Promise<string>;
 
   return prediction;
 };
